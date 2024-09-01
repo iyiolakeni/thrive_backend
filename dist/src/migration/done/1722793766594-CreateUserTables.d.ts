8107b284ab1cd942693109b3d563b0fa3ec9dd88
iyiolakeni <iyiolakeni@gmail.com>
2024-09-01 07:09:13 +0100

    Remove unused migration files for creating user and business tables

--- dist/src/migration/done/1722793766594-CreateUserTables.d.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class CreateUserTables1722793766594 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
-//# sourceMappingURL=1722793766594-CreateUserTables.d.ts.map
